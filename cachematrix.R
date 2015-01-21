453dc92e094770aef348222385cb7f97a70b4bea
David Lynch <dave@soundwave.com>
2015-01-21 17:00:45 +0000

    First cut of solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,64 @@
-## Put comments here that give an overall description of what your
-## functions do
+## David Lynch - Assignment 2
+## These functions allow the creation of a matrix instance that will cache the result of
+## its inverse
 
-## Write a short comment describing this function
-
+# Create a function that wraps X and stores some 
+# data that persists over multiple invocations of X
+#
+# Also expose some functions that allow retrieval and modification of that state
 makeCacheMatrix <- function(x = matrix()) {
+  ## Variable to store the inverse
+  i <- NULL
+  
+  ## The number of cache hits so far (useful for testing)
+  hits <- 0
+  
+  # Set the matrix to something new, unsetting the cached inverse
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+    hits <<- 0
+  }
+  
+  # Return the number of hits
+  gethits <- function() hits
 
+  # Increment the cache hit count
+  inchits <- function() hits <<- hits + 1
+
+  # Return the original matrix
+  get <- function() x
+  
+  # Set the inverse of X
+  setinv <- function(iv) i <<- iv
+  
+  # Get the cached inverse 
+  getinv <- function() i
+  
+  list(set = set, get = get,
+       gethits = gethits,
+       inchits = inchits,
+       getinv = getinv,
+       setinv= setinv)
 }
 
 
-## Write a short comment describing this function
-
+## Use solve to set the inverse of a matrix
+## Behaviour of this function is undefined if the matrix is not invertable
+## It's most likely to stop with an error
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    # Set i as the inverse - this will be null if not yet set
+    i <- x$getinv()
+    
+    # If it is not null - we have a cache hit
+    if(!is.null(i)) {
+        message("Cache Hit!")
+        x$inchits()  # Counting these for test purposes
+        return(i)
+    }
+    # We need to calculate the inverse, since cache is null
+    data <- x$get()     # Get the data
+    i <- solve(data)    # Get the inverse
+    x$setinv(i)         # Cache the inverse 
+    i                   # Return the inverse
 }
---
+++ cachematrix_test.R
@@ -0,0 +1,61 @@
+# Test using a hardcoded invertible 3x3 matrix
+# This test will invert the matrix 3 times, expecting to hit the cache twice
+# This test will then invert the result and compare to original - to test the inversion works
+# Matrix is assumed invertible.
+#
+# Returns TRUE if test passes
+test_3by3_matrix <- function() {
+    # Load our functions under test
+    source("cacheamatrix.R")
+    
+    # An invertable matrix
+    x <- c(1,0,5)
+    y <- c(2,1,6)
+    z <- c(3,4,0)
+    mat <- cbind(x, y, z)
+    
+    # The inverse of the above matrix, with dimnames inverted too
+    x <- c(-24, 18, 5)
+    y <- c(20, -15, -4)
+    z <- c(-5, 4, 1)
+    expected <- rbind(x, y, z)
+    
+    # Create the cacheable matrix for the test
+    cache_m <- makeCacheMatrix(mat)
+    
+    # Solve this once 
+    first_i <- cacheSolve(cache_m)
+    # Next time around - we expect a cache hit
+    second_i <- cacheSolve(cache_m)
+    # Once more for luck - another cache hit
+    third_i <- cacheSolve(cache_m)
+    
+    # Lets go backwards to test our inverse function
+    cache_m_inv <- makeCacheMatrix(expected)
+    exp_inv <- cacheSolve(cache_m_inv)
+    
+    first_equal <- FALSE
+    second_equal <- FALSE
+    third_equal <- FALSE
+    inv_of_inv <- FALSE 
+    
+    expected_hits <- 2
+    
+    if(all.equal(first_i, expected)) {
+      first_equal <- TRUE
+    }
+    if(all.equal(second_i, expected)) {
+      second_equal <- TRUE
+    }
+    if(all.equal(third_i, expected)) {
+      third_equal <- TRUE
+    }
+    if(all.equal(mat, exp_inv)) {
+      inv_of_inv <- TRUE
+    }
+    return(first_equal&&    # We get expected result first run
+           second_equal&&   # ...and second run
+           third_equal&&    # ...and third run
+           inv_of_inv&&     # The inverse of the inverse should be the original
+          (expected_hits==cache_m$gethits()))  # The expected cache hits should be equal
+}
